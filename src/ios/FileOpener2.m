2a903fdae36b3f2ea6e5dbf6c67568328fe34166
Marvin Martinez <marvin@thelinkapp.co.uk>
2017-03-20 11:28:57 +0000

    Replaced the iOS source files to make it work.

--- src/ios/FileOpener2.m
+++ src/ios/FileOpener2.m
@@ -1,25 +1,21 @@
 /*
-The MIT License (MIT)
-
-Copyright (c) 2013 pwlin - pwlin05@gmail.com
-
-Permission is hereby granted, free of charge, to any person obtaining a copy of
-this software and associated documentation files (the "Software"), to deal in
-the Software without restriction, including without limitation the rights to
-use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
-the Software, and to permit persons to whom the Software is furnished to do so,
-subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in all
-copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
-FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
-COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
-IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
-CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-*/
+   The MIT License (MIT)
+   Copyright (c) 2013 pwlin - pwlin05@gmail.com
+   Permission is hereby granted, free of charge, to any person obtaining a copy of
+   this software and associated documentation files (the "Software"), to deal in
+   the Software without restriction, including without limitation the rights to
+   use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+   the Software, and to permit persons to whom the Software is furnished to do so,
+   subject to the following conditions:
+   The above copyright notice and this permission notice shall be included in all
+   copies or substantial portions of the Software.
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+   IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+   FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+   COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+   IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+   CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+   */
 #import "FileOpener2.h"
 #import <Cordova/CDV.h>
 
@@ -27,55 +23,79 @@ CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #import <MobileCoreServices/MobileCoreServices.h>
 
 @implementation FileOpener2
+@synthesize controller = docController;
 
 - (void) open: (CDVInvokedUrlCommand*)command {
 
-    NSString *path = command.arguments[0];
-    NSString *uti = command.arguments[1];
-    if (!uti || (NSNull*)uti == [NSNull null]) {
-        NSArray *dotParts = [path componentsSeparatedByString:@"."];
-        NSString *fileExt = [dotParts lastObject];
-        
-        uti = (__bridge NSString *)UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)fileExt, NULL);
-    }
+	NSString *path = [[command.arguments objectAtIndex:0] stringByAddingPercentEscapesUsingEncoding:NSUTF8StringEncoding];
+	NSString *contentType = nil;
 
-    CDVViewController* cont = (CDVViewController*)[ super viewController ];
+	if([command.arguments count] == 2) { // Includes contentType
+		contentType = [command.arguments objectAtIndex:1];
+	}
 
-    dispatch_async(dispatch_get_main_queue(), ^{
-        // TODO: test if this is a URI or a path
-        NSURL *fileURL = [NSURL URLWithString:path];
-        
-        localFile = fileURL.path;
-        
-        NSLog(@"looking for file at %@", fileURL);
-        NSFileManager *fm = [NSFileManager defaultManager];
-        if(![fm fileExistsAtPath:localFile]) {
-            NSDictionary *jsonObj = @{@"status" : @"9",
-                                      @"message" : @"File does not exist"};
-            CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                                                          messageAsDictionary:jsonObj];
-            [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-            return;
-        }
+	CDVViewController* cont = (CDVViewController*)[ super viewController ];
+	self.cdvViewController = cont;
 
-        self.controller = [UIDocumentInteractionController  interactionControllerWithURL:fileURL];
-        self.controller.delegate = self;
-        self.controller.UTI = uti;
+	NSArray *dotParts = [path componentsSeparatedByString:@"."];
+	NSString *fileExt = [dotParts lastObject];
 
-        CGRect rect = CGRectMake(0, 0, 1000.0f, 150.0f);
-        CDVPluginResult* pluginResult = nil;
-        BOOL wasOpened = [self.controller presentOptionsMenuFromRect:rect inView:cont.view animated:NO];
+	NSString *uti = (__bridge NSString *)UTTypeCreatePreferredIdentifierForTag(kUTTagClassFilenameExtension, (__bridge CFStringRef)fileExt, NULL);
 
-        if(wasOpened) {
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @""];
-        } else {
-            NSDictionary *jsonObj = @{@"status" : @"9",
-                                      @"message" : @"Could not handle UTI"};
-            pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR
-                                         messageAsDictionary:jsonObj];
-        }
-        [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    });
+	BOOL isPreview = NO;
+
+	if(contentType) {
+		if([contentType isEqualToString: @"application/pdf"]) {
+			isPreview = YES;
+		}
+		
+		if([contentType hasPrefix: @"image"]) {
+			isPreview = YES;
+		}
+	}
+
+	dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+			
+			NSURL *fileURL = nil;
+			
+			fileURL = [NSURL URLWithString:path];
+			
+			localFile = fileURL.path;
+
+			dispatch_async(dispatch_get_main_queue(), ^{
+
+					docController = [UIDocumentInteractionController  interactionControllerWithURL:fileURL];
+					docController.delegate = self;
+					docController.UTI = uti;
+
+					CGRect rect = CGRectMake(0, 0, 1000.0f, 150.0f);
+					CDVPluginResult* pluginResult = nil;
+					
+					//Abre o preview do arquivo
+					BOOL wasOpened = [docController presentPreviewAnimated: NO];
+
+					if(wasOpened) {
+						pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString: @""];
+						//NSLog(@"Success");
+					} else {
+						NSDictionary *jsonObj = [ [NSDictionary alloc]
+							initWithObjectsAndKeys :
+							@"9", @"status",
+							@"Could not handle UTI", @"message",
+							nil
+						];
+						pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsDictionary:jsonObj];
+					}
+					[self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
+			});
+	});
 }
+
+@end
+
+@implementation FileOpener2 (UIDocumentInteractionControllerDelegate)
+	- (UIViewController *)documentInteractionControllerViewControllerForPreview:(UIDocumentInteractionController *)controller {
+		return self.cdvViewController;
+	}
 
 @end
